eb99ee5f2be7aa82a47aef0759107704d35a6a55
Glitch (buttery-zigzag-salad) <none>
2021-08-28 14:34:33 +0000

    🏤😼 Checkpoint ./komutlar/çalış.js:31453801/1211

--- komutlar/çalış.js
+++ komutlar/çalış.js
@@ -1,35 +1,34 @@
 const Discord = require('discord.js')
 const db = require('quick.db')
-const ms = require('ms');
+const ms = require('parse-ms');
 
 exports.run = async (bot, message, args) => { 
 
-    var bakiye = db.fetch(`bakiye_${message.author.id}`)
+    let para = db.fetch(`bakiye_${message.author.id}`)
     let kullanıcı = message.author;
     let sure = await db.fetch(`calissüre_${message.member.id}`)
-
+  
+    function rastgeleMiktar(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+    }    
     let cd = 600000;
     
     if (sure !== null && cd - (Date.now() - sure) > 0) {
         let timeObj = ms(cd - (Date.now() - sure));
     
        let timeEmbed = new Discord.RichEmbed()
-        .setColor("#eec400")
         .setDescription(`Bir daha çalışmak için biraz beklemen gerekli **${timeObj.second} saniye** sonra tekrar dene!`);
         message.channel.send(timeEmbed)
       } else {
 
-        const cevap = ["Yazılımcılık","","Kolyoz tuttun","İstavrit tuttun","Köpek balığı tuttun"]
-        var Cevap = cevap[Math.floor(Math.random() * cevap.length)]
-
-        var sonuç = Math.floor((Math.random() * cevap.length));
-        let bakiye = Math.floor(Math.random() * 3900) + 1
-        let embed = new Discord.RichEmbed()
-        .setColor("#eec400")
-        .setDescription(`${Cevap} yaparak siparişini tamamladığın şirket sana ${bakiye} 💸 ödedi!`);
-        message.channel.send(embed)
+        const Cevap = ["Yazılımcılık"]
+        var cevap = Cevap[Math.floor(Math.random() * Cevap.length)]
+        var kazandın = rastgeleMiktar(400,3900)
+        message.channel.send(`${Cevap} yaparak siparişini tamamladığın şirket sana ${kazandın} 💸 ödedi!`)
         
-        db.add(`bakiye_${message.guild.id}_${kullanıcı.id}`, bakiye)
+        db.add(`bakiye_${message.guild.id}_${kullanıcı.id}`, kazandın)
         db.set(`çalışma_${message.guild.id}_${kullanıcı.id}`, Date.now())
     };
 }
@@ -43,5 +42,5 @@ exports.conf = {
 module.exports.help = {
   name:"çalışma",
   description: 'çalışma',
-  usage: 'çalışma',
-}
+  usage: 'çalışma'
+  }
