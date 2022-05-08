72463401ce9cffd86e70e88d8638ab683f117f55
Glitch (buttery-zigzag-salad) <none>
2022-05-08 16:35:14 +0000

    🎬🙈 Checkpoint ./komutlar/yumruk.js:23597830/564

--- komutlar/yumruk.js
+++ komutlar/yumruk.js
@@ -2,9 +2,11 @@ const Discord = require('discord.js')
 exports.run = function(bot, message) {
     message.channel.send(new Discord.MessageEmbed()
     .setColor('RANDOM')
-    .setDescription('> Sayınız Yukarıda :)')
+    .setDescription(``)                   
     .setFooter(message.author.tag, message.author.displayAvatarURL())
+    .setimage(`https://tenor.com/view/grab-and-punch-grab-n-punch-punching-punch-punch-up-gif-15401036`)
     .setTitle('Yumruk atma işlemin: ' + doMagicDiceVoodoo()));
+    
 
     function doMagicDiceVoodoo() {
         var rand = ['Eğilerek Yumruktan Sıyrıldı','Sol Yanağına Sert Bir Yumruk Attın!','Yüzünün Ortasına Bir Yumruk Attın ve Bayılttın!','Sağ Yanağına Sert Bir Yumruk Attın!'];
