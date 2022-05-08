5743d14e1b5c5077438226b24c13154b6ae49f49
Glitch (buttery-zigzag-salad) <none>
2022-05-08 16:47:19 +0000

    🐁💐 Checkpoint ./komutlar/yumruk.js:23597830/5806

--- komutlar/yumruk.js
+++ komutlar/yumruk.js
@@ -4,12 +4,11 @@ exports.run = function(bot, message) {
     .setColor('RANDOM')
     .setDescription(``)                   
     .setFooter(message.author.tag, message.author.displayAvatarURL())
-    .setimage(`https://tenor.com/view/grab-and-punch-grab-n-punch-punching-punch-punch-up-gif-15401036`)
     .setTitle('Yumruk atma işlemin: ' + doMagicDiceVoodoo()));
     
 
     function doMagicDiceVoodoo() {
-        var rand = ['Eğilerek Yumruktan Sıyrıldı','Sol Yanağına Sert Bir Yumruk Attın!','Yüzünün Ortasına Bir Yumruk Attın ve Bayılttın!','Sağ Yanağına Sert Bir Yumruk Attın!'];
+        var rand = ['Eğilerek Yumruktan Sıyrıldı','Sol Yanağına Sert Bir Yumruk Attın!','Yüzünün Ortasına Bir Yumruk Attın','Sağ Yanağına Sert Bir Yumruk Attın!','Yana Çekildi Ve Yumruktan Sıyrıldı!','Yumruğu Atacakken Elini Tuttu Ve Atamadın!'];
 
         return rand[Math.floor(Math.random()*rand.length)];
     }
